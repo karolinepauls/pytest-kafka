80b5dc21dfdb892fc722fafad4659cdd29cfc4c2
Karoline Pauls <code@karolinepauls.com>
2021-08-24 23:14:31 +0100

    Version 0.5.0

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ class DownloadKafka(Command):
         set_up_kafka()
 
 
-VERSION = '0.4.0'
+VERSION = '0.5.0'
 
 # Optional suffix for Test PyPI packages.
 VERSION_SUFFIX = environ.get('VERSION_SUFFIX', None)
