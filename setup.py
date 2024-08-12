7723ce7a8acaaa7f5db4c34d03f9fe599f5fc74f
Karoline Pauls <code@karolinepauls.com>
2024-08-12 23:27:57 +0100

    Version 0.7.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from setuptools import setup  # type: ignore
 
 
-VERSION = '0.7.0'
+VERSION = '0.7.1'
 
 # Optional suffix for Test PyPI packages.
 VERSION_SUFFIX = environ.get('VERSION_SUFFIX', None)
